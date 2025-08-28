9c18892ceef328ddf7c844e027aa546cb96d71ed
winalife-kp <karol.perec@winalife.com>
2025-08-28 17:28:05 +0200

    remove provider import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChannelService } from './channel.service';
-import { KafkaLibService } from './kafka.service';
 
 @Module({
   imports: [],
